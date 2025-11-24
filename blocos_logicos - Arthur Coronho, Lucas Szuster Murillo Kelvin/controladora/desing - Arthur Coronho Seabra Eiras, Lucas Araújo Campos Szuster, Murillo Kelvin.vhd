cf189deeca389de8d11f8f537a15437ca2ab1146
Seabrinhagemaplys <66424663+Seabrinhagemaplys@users.noreply.github.com>
2025-11-24 19:22:19 -0300

    Enhance controladora with state vectors and logic
    Added state vector outputs and improved state handling.

--- blocos_logicos - Arthur Coronho, Lucas Szuster Murillo Kelvin/controladora/desing - Arthur Coronho Seabra Eiras, Lucas Araújo Campos Szuster, Murillo Kelvin.vhd
+++ blocos_logicos - Arthur Coronho, Lucas Szuster Murillo Kelvin/controladora/desing - Arthur Coronho Seabra Eiras, Lucas Araújo Campos Szuster, Murillo Kelvin.vhd
@@ -19,7 +19,9 @@ entity controladora is
         sinal_amarelo   : out std_logic;
         sinal_vermelho  : out std_logic;
         night           : out std_logic;
-        clear_contador  : out std_logic
+        clear_contador  : out std_logic;
+        vetor_estado    : out std_logic_vector (2 downto 0);
+        vetor_proximo_estado : out std_logic_vector (2 downto 0)
     );
 end entity;
 
@@ -50,63 +52,90 @@ begin
     end process;
    
     -- Cálculo das saídas do estado atual
-    process(reset, emergencia, count, outra_emergencia, ativar_night, pedestre, outro_pedestre)
+    process(reset, emergencia, count, outra_emergencia, ativar_night, pedestre, outro_pedestre, estado_atual)
     begin 
         case estado_atual is
             when ESTADO_SINAL_VERMELHO =>
-                if (emergencia = '1' or count = '1') then
-                    proximo_estado <= ESTADO_RESETAR_SINAL_VERDE;
-                elsif (ativar_night = '1') then
+                if (ativar_night = '1') then
+                	report "segundo if do estado sinal vermelho.";
                     proximo_estado <= ESTADO_NIGHT;
+                    vetor_proximo_estado <= "101";
+                elsif (outra_emergencia = '1') then
+                	proximo_estado <= ESTADO_RESETAR_SINAL_VERMELHO;
+                    vetor_proximo_estado <= "100";
+                elsif (emergencia = '1' or count = '1') then
+                	report "primeiro if do estado sinal vermelho";
+                    proximo_estado <= ESTADO_RESETAR_SINAL_VERDE;
+                    vetor_proximo_estado <= "011";
                 end if;
 
             when ESTADO_SINAL_AMARELO =>
                 if ativar_night = '1' then 
                     proximo_estado <= ESTADO_NIGHT;
+                    vetor_proximo_estado <= "101";
                 elsif emergencia = '1' then
                     proximo_estado <= ESTADO_RESETAR_SINAL_VERDE;
+                    vetor_proximo_estado <= "011";
                 else
                     proximo_estado <= ESTADO_RESETAR_SINAL_VERMELHO;
+                    vetor_proximo_estado <= "100";
                 end if;
 
             when ESTADO_SINAL_VERDE =>
                 if (emergencia = '1') then
                     proximo_estado <= ESTADO_RESETAR_SINAL_VERDE;
+                    vetor_proximo_estado <= "011";
                 elsif (pedestre = '1' or outra_emergencia = '1') then
                     proximo_estado <= ESTADO_RESETAR_SINAL_VERMELHO;
+                    vetor_proximo_estado <= "100";
                 elsif (count = '1') then
                     proximo_estado <= ESTADO_SINAL_AMARELO;
+                    vetor_proximo_estado <= "001";
                 elsif (ativar_night = '1') then
                     proximo_estado <= ESTADO_NIGHT;
+                    vetor_proximo_estado <= "101";
                 end if;
 
             when ESTADO_RESETAR_SINAL_VERDE =>
                 if ativar_night = '1' then
                     proximo_estado <= ESTADO_NIGHT;
+                    vetor_proximo_estado <= "101";
                 elsif emergencia = '1' then
                     proximo_estado <= ESTADO_RESETAR_SINAL_VERDE;
+                    vetor_proximo_estado <= "011";
                 elsif (pedestre = '1') or (outra_emergencia = '1') then
                     proximo_estado <= ESTADO_RESETAR_SINAL_VERMELHO;
-                elsif count = '1' then
-                    proximo_estado <= ESTADO_SINAL_AMARELO;
+                    vetor_proximo_estado <= "100";
+                elsif emergencia = '0' then
+                    proximo_estado <= ESTADO_SINAL_VERDE;
+                    vetor_proximo_estado <= "000";
                 end if;
 
             when ESTADO_RESETAR_SINAL_VERMELHO =>
                 if (emergencia = '1') then
+                	report "primeiro if";
                     proximo_estado <= ESTADO_RESETAR_SINAL_VERDE;
+                    vetor_proximo_estado <= "011";
                 elsif (outra_emergencia = '1') then
+                	report "Ssegundo if.";
                     proximo_estado <= ESTADO_RESETAR_SINAL_VERMELHO;
-                elsif (outra_emergencia = '0') then
-                    proximo_estado <= ESTADO_SINAL_VERMELHO;
+                    vetor_proximo_estado <= "100";
                 elsif (ativar_night = '1') then    
+                    report "quarto if";
                     proximo_estado <= ESTADO_NIGHT;
+                    vetor_proximo_estado <= "101";
+                else 
+                	proximo_estado <= ESTADO_SINAL_VERMELHO;
+                    vetor_proximo_estado <= "010";
                 end if;
 
             when ESTADO_NIGHT =>
                 if (ativar_night = '1') then
                     proximo_estado <= ESTADO_NIGHT;
+                    vetor_proximo_estado <= "101";
                 elsif (ativar_night = '0') then
                     proximo_estado <= ESTADO_SINAL_VERDE;
+                    vetor_proximo_estado <= "000";
                 end if;
         end case;
     end process;
@@ -121,6 +150,7 @@ begin
                 sinal_vermelho <= '1';
                 night          <= '0';
                 clear_contador <= '0';
+                vetor_estado <= "010";
 
             when ESTADO_SINAL_AMARELO =>
                 sinal_verde    <= '0';
@@ -128,6 +158,7 @@ begin
                 sinal_vermelho <= '0';
                 night          <= '0';
                 clear_contador <= '0';
+                vetor_estado <= "001";
 
             when ESTADO_SINAL_VERDE =>
                 sinal_verde    <= '1';
@@ -135,6 +166,7 @@ begin
                 sinal_vermelho <= '0';
                 night          <= '0';
                 clear_contador <= '0';
+                vetor_estado <= "000";
 
             when ESTADO_RESETAR_SINAL_VERDE =>
                 sinal_verde    <= '1';
@@ -142,6 +174,7 @@ begin
                 sinal_vermelho <= '0';
                 night          <= '0';
                 clear_contador <= '1';
+                vetor_estado <= "011";
 
             when ESTADO_RESETAR_SINAL_VERMELHO =>
                 sinal_verde    <= '0';
@@ -149,6 +182,7 @@ begin
                 sinal_vermelho <= '1';
                 night          <= '0';
                 clear_contador <= '1';
+                vetor_estado <= "100";
 
             when ESTADO_NIGHT =>
                 sinal_verde    <= '0';
@@ -156,6 +190,7 @@ begin
                 sinal_vermelho <= '0';
                 night          <= '1';
                 clear_contador <= '0';
+                vetor_estado <= "101";
 
             when others =>
                 sinal_verde    <= '0';
@@ -163,6 +198,7 @@ begin
                 sinal_vermelho <= '0';
                 night          <= '0';
                 clear_contador <= '0';
+                vetor_estado <= "111";
         end case;
     end process;
 end architecture;
